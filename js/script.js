8cdfa4b202549eb35e548e12a3b0ed3c627218b8
Vincent Fox <siiin@inbox.ru>
2019-11-11 02:07:29 +0300

    final

--- js/script.js
+++ js/script.js
@@ -12,12 +12,18 @@ jQuery.fn.cardGame = function(data) {
         $back = $("#card-back", $card),
         questionTmpl = _.template($("#question__tmpl").html()),
         answerTmpl = _.template($("#answer__tmpl").html()),
+        resultTmpl = _.template($("#result__tmpl").html()),
         _balance = 100,
         _profit = 0;
 
     function updateResult(answer) {
         _balance += answer.balance;
         _profit += answer.profit;
+    }
+
+    function resetResult() {
+        _balance = 100;
+        _profit = 0;
     }
 
     function getResult() {
@@ -36,8 +42,38 @@ jQuery.fn.cardGame = function(data) {
         var $this = $(e.currentTarget),
             questionId = $this.data("question");
 
-        $front.html(questionTmpl(_.extend(questionsData[questionId], {id: questionId}, getResult())));
+        if (_balance <= 0) {
+            $front.html(resultTmpl({
+                type: 'low',
+                buttonText: 'Начать сначала',
+                text: 'Упс, деньги закончились.<br/>Попробуйте начать сначала )))'
+            }));
+
+            resetResult();
+
+        } else  if (questionId === 'result') {
+            var data = {
+                type:  'medium',
+                buttonText: 'Попробовать еще раз',
+                text: 'Неплохо! Компания не&nbsp;достигла плановой прибыли, но&nbsp;Вы смогли не&nbsp;допустить банкротства, и&nbsp;сохранить деньги на&nbsp;счетах! Попробуйте еще раз, если хотите достичь амбициозных целей собственника!'
+            };
+
+            if (_profit >= 7) {
+                data = {
+                    type:  'high',
+                    buttonText: 'Попробовать еще раз',
+                    text: 'Браво! Вы&nbsp;блестяще справились!<br/>Бизнес процветает, и&nbsp;все это благодаря Вам!'
+                };
+            }
+
+            resetResult();
+
+            $front.html(resultTmpl(data));
+        } else {
+            $front.html(questionTmpl(_.extend(questionsData[questionId], {id: questionId}, getResult())));
+        }
         $card.flip(false);
+
 
     });
 
